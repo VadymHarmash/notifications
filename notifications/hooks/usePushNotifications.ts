a8f6e5fd8ca70da139d1991ad73e883471b10641
Дар`я Бількевич <ipz202_bdv@student.ztu.edu.ua>
2024-10-29 13:54:39 +0200

    Updated expoToken hook

--- notifications/hooks/usePushNotifications.ts
+++ notifications/hooks/usePushNotifications.ts
@@ -1,76 +1,73 @@
-import { useState, useEffect, useRef } from "react";
-import { Platform } from "react-native";
+import { useState, useEffect, useRef } from 'react';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
-import * as Device from "expo-device";
-import * as Notifications from "expo-notifications";
+import Constants from 'expo-constants';
 
-import Constants from "expo-constants";
+import { Platform } from 'react-native';
 
-interface IPushNotifications {
-  notification?: Notifications.Notification;
+export interface PushNotificationState {
   expoPushToken?: Notifications.ExpoPushToken;
+  notification?: Notifications.Notification;
 }
 
-export const usePushNotifications = (): IPushNotifications => {
+export const usePushNotifications = (): PushNotificationState => {
   Notifications.setNotificationHandler({
     handleNotification: async () => ({
-      shouldShowAlert: true,
       shouldPlaySound: false,
+      shouldShowAlert: true,
       shouldSetBadge: false,
     }),
   });
-
-  const [notification, setNotification] = useState<
-    Notifications.Notification | undefined
-  >();
 
   const [expoPushToken, setExpoPushToken] = useState<
     Notifications.ExpoPushToken | undefined
   >();
 
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >();
+
   const notificationListener = useRef<Notifications.Subscription>();
   const responseListener = useRef<Notifications.Subscription>();
 
-  async function registrationForPushNotifications() {
+  async function registerForPushNotificationsAsync() {
     let token;
-
     if (Device.isDevice) {
       const { status: existingStatus } =
         await Notifications.getPermissionsAsync();
-
       let finalStatus = existingStatus;
 
-      if (existingStatus !== "granted") {
+      if (existingStatus !== 'granted') {
         const { status } = await Notifications.requestPermissionsAsync();
         finalStatus = status;
       }
-
-      if (finalStatus !== "granted") {
-        alert("Failed to get push token");
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification');
         return;
       }
 
       token = await Notifications.getExpoPushTokenAsync({
-        projectId: Constants.expo?.extra?.eas?.projectId,
+        projectId: Constants.expoConfig?.extra?.eas.projectId,
       });
-
-      if (Platform.OS === "android") {
-        await Notifications.setNotificationChannelAsync("default", {
-          name: "default",
-          importance: Notifications.AndroidImportance.MAX,
-          vibrationPattern: [0, 250, 250, 250],
-          lightColor: "#FF231F7C",
-        });
-      }
-
-      return token;
     } else {
-      console.log("Use a physical device");
+      alert('Must be using a physical device for Push notifications');
     }
+
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+
+    return token;
   }
 
   useEffect(() => {
-    registrationForPushNotifications().then((token) => {
+    registerForPushNotificationsAsync().then((token) => {
       setExpoPushToken(token);
     });
 
@@ -79,15 +76,14 @@ export const usePushNotifications = (): IPushNotifications => {
         setNotification(notification);
       });
 
-    responseListener.current = Notifications.addNotificationReceivedListener(
-      (response) => {
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
         console.log(response);
-      },
-    );
+      });
 
     return () => {
       Notifications.removeNotificationSubscription(
-        notificationListener.current!,
+        notificationListener.current!
       );
 
       Notifications.removeNotificationSubscription(responseListener.current!);
